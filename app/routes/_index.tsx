6158cfb0aecad2e26298c1cfac10ef8e4ff33966
Scotty.wkc <scotty.wkc@gmail.com>
2025-02-23 18:22:58 +1100

    initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,22 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1>Hello, World!</h1>
+
+        </header>
+
+      </div>
+    </div>
+  );
+}
